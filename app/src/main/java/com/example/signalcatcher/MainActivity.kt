d7d372c6aa6451d46266df0645f956b6e56740ef
jw3520 <55908720+jw3520@users.noreply.github.com>
2020-01-05 20:02:14 +0900

    ListActivity

--- app/src/main/java/com/example/signalcatcher/MainActivity.kt
+++ app/src/main/java/com/example/signalcatcher/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.signalcatcher
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val nextIntent = Intent(this, ListActivity::class.java)
+            startActivity(nextIntent)
+            finish()
+        }
     }
 }
